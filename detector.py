9d4985459271ab45211c5d3de68b0ddd0707daf5
Raghul Thangavel <46341793+raghulrage@users.noreply.github.com>
2020-02-03 21:30:30 +0530

    Add files via upload

---
+++ detector.py
@@ -0,0 +1,40 @@
+import cv2
+import numpy as np
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read('trainner/trainner.yml')
+cascadePath = "haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascadePath);
+list1=[]
+
+cam = cv2.VideoCapture(0)
+font = cv2.FONT_HERSHEY_SIMPLEX
+while True:
+    ret, frame =cam.read()
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    faces=faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30)
+    )
+    for(x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,255,255),3)
+        Id, conf = recognizer.predict(gray[y:y+h,x:x+w])
+        print(Id,conf)
+        if(conf <55 ):
+            if(Id==1):
+                Id='Raghul'
+            if(Id==3):
+                Id='Jeeva'
+            list1.append(Id)
+        else:
+            Id="Unknown"
+        cv2.putText(frame,str(Id), (x,y+h),font,1, (0,255,0),2)
+    cv2.imshow('frame',frame)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27: # press 'ESC' to quit
+        break
+cam.release()
+print(set(list1))
+cv2.destroyAllWindows()
