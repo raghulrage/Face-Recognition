3c9cb2302d9c6d37c4128150255727cf4b297c80
Raghul Thangavel <46341793+raghulrage@users.noreply.github.com>
2020-05-15 15:26:39 +0530

    Add files via upload

--- detector.py
+++ detector.py
@@ -5,7 +5,6 @@ recognizer = cv2.face.LBPHFaceRecognizer_create()
 recognizer.read('trainner/trainner.yml')
 cascadePath = "haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cascadePath);
-list1=[]
 
 cam = cv2.VideoCapture(0)
 font = cv2.FONT_HERSHEY_SIMPLEX
@@ -22,13 +21,7 @@ while True:
         cv2.rectangle(frame,(x,y),(x+w,y+h),(255,255,255),3)
         Id, conf = recognizer.predict(gray[y:y+h,x:x+w])
         print(Id,conf)
-        if(conf <55 ):
-            if(Id==1):
-                Id='Raghul'
-            if(Id==3):
-                Id='Jeeva'
-            list1.append(Id)
-        else:
+        if(conf >55 ):
             Id="Unknown"
         cv2.putText(frame,str(Id), (x,y+h),font,1, (0,255,0),2)
     cv2.imshow('frame',frame)
@@ -36,5 +29,4 @@ while True:
     if k == 27: # press 'ESC' to quit
         break
 cam.release()
-print(set(list1))
 cv2.destroyAllWindows()
