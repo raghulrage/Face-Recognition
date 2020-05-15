8b8063d33306ccb0e11b84c4bee92259a6f58ba7
Raghul Thangavel <46341793+raghulrage@users.noreply.github.com>
2020-05-15 16:45:49 +0530

    Update createDataset.py

--- createDataset.py
+++ createDataset.py
@@ -1,29 +1,34 @@
 import cv2
 
-cam = cv2.VideoCapture(0)
-detector=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+camera = cv2.VideoCapture(0)
+detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-Id=input('enter your id')
-sampleNum=0
-while(True):
-    ret, img = cam.read()
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = detector.detectMultiScale(gray, 1.2, 5)
-    for (x,y,w,h) in faces:
-        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+Id = int(input('Enter an Id: '))
 
-        #incrementing sample number
-        sampleNum=sampleNum+1
-        #saving the captured face in the dataset folder 
-        cv2.imwrite("dataset/User."+Id +'.'+ str(sampleNum) + ".jpg", gray[y:y+h,x:x+w])
+itrId = 0
 
-        cv2.imshow('frame',img)
-    #wait for 100 miliseconds
-    if cv2.waitKey(100) & 0xFF == ord('q'):
+while True:
+    
+    ret,image = camera.read()
+    gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+
+    face = detector.detectMultiScale( gray, scaleFactor = 1.3, minNeighbors = 5)
+
+    itrId += 1
+
+    for (x,y,w,h) in face:
+        cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,0),2)
+
+        cv2.imwrite('dataset/User.'+str(Id)+'.'+str(itrId)+'.jpg',gray[y:y+h,x:x+w])
+
+        cv2.imshow('Video',image)
+
+    if cv2.waitKey(100) and 0xFF == 27:
         break
-    # break if the sample number is morethan 20
-    elif sampleNum>20:
+
+    if itrId > 20:
         break
-os.system('face_recognizer.py')
-cam.release()
+
+camera.release()
 cv2.destroyAllWindows()
+
