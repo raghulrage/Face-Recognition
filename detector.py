8a29ec0dd0d9ac920239a7caef81a2d0e843e70b
Raghul Thangavel <46341793+raghulrage@users.noreply.github.com>
2020-05-15 16:46:26 +0530

    Update detector.py

--- detector.py
+++ detector.py
@@ -1,32 +1,34 @@
 import cv2
-import numpy as np
+
+camera = cv2.VideoCapture(0)
+detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 recognizer.read('trainner/trainner.yml')
-cascadePath = "haarcascade_frontalface_default.xml"
-faceCascade = cv2.CascadeClassifier(cascadePath);
 
-cam = cv2.VideoCapture(0)
-font = cv2.FONT_HERSHEY_SIMPLEX
 while True:
-    ret, frame =cam.read()
-    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    faces=faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30)
-    )
-    for(x,y,w,h) in faces:
-        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,255,255),3)
-        Id, conf = recognizer.predict(gray[y:y+h,x:x+w])
-        print(Id,conf)
-        if(conf >55 ):
-            Id="Unknown"
-        cv2.putText(frame,str(Id), (x,y+h),font,1, (0,255,0),2)
-    cv2.imshow('frame',frame)
-    k = cv2.waitKey(30) & 0xff
-    if k == 27: # press 'ESC' to quit
+    ret, img = camera.read()
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    face = detector.detectMultiScale(gray,scaleFactor = 1.3, minNeighbors = 5, minSize = (30,30))
+
+    for (x,y,w,h) in face:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,255),2)
+
+        faceid, percentage = recognizer.predict(gray[y:y+h,x:x+w])
+
+        if percentage > 50:
+            text = 'Unknown'
+        else:
+            text = str(faceid)
+
+        cv2.putText(img, text, (x,y+h), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
+
+    cv2.imshow('Video',img)
+
+    if cv2.waitKey(100) & 0xFF == 27:
         break
-cam.release()
-cv2.destroyAllWindows()
+
+camera.release()
+cv2.destroyAlLWindows()
+
+
